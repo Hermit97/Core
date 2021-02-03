3199300ff3286cec2b4ccf10d36d6fc1bef20e88
ShawnAli97 <shawn10ali@gmail.com>
2021-02-03 02:56:19 -0500

    error logic complete

--- src/two-digit-game/numbers.java
+++ src/two-digit-game/numbers.java
@@ -1,5 +1,22 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class numbers{
 	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		int userNumber;
+
+		try{
+
+		System.out.println("Enter a two digit number. The two digits should be different. ");
+		userNumber = input.nextInt();
+		}
+		catch(InputMismatchException exception){
+			System.out.println("Not an integer! Try again!");
+		}
+
+		input.close();
 
 	}
 }
