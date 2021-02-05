8f7c099853510550644417e3fcac73422ec35eb9
ShawnAli97 <shawn10ali@gmail.com>
2021-02-05 15:02:25 -0500

    Its back

--- src/Hollow_shape/shape.java
+++ src/Hollow_shape/shape.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 public class shape{
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
 
-		int howMany;
-		char userChar;
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	int howMany;
+	char userChar;
+	char startGame;
+
+	System.out.println("Do you want to start(Y/N)");
+	startGame = input.next().charAt(0);
+
+
 
 	}
 }
--- src/two-digit-game/numbers.java
+++ src/two-digit-game/numbers.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class numbers{
-    public static void main(String[] args){
+    public static void main(String args[]){
 
 	Scanner input = new Scanner(System.in);
 	int userNumber = 1;
