3ff72aca972a303f3cb4bdecefed961bed4506c6
ShawnAli97 <shawn10ali@gmail.com>
2021-02-05 16:21:01 -0500

    error handleing added

--- src/Hollow_shape/shape.java
+++ src/Hollow_shape/shape.java
@@ -10,7 +10,21 @@ public class shape{
 	System.out.println("Do you want to start(Y/N)");
 	startGame = input.next().charAt(0);
 
+	while(startGame == 'y' || startGame == 'Y'){
+		try{
+			
+			System.out.println("How many chars/last row?");
+			howMany = input.nextInt();
+			
+			if(howMany < 5 || howMany > 21)
+				System.out.println("Error! Valid range 5 - 21.");
+		}
+		catch(Exception e){
+			System.out.print("Not an integer! Try again!");
+			input.next();
+			}
 
-
+		
+		}
 	}
 }
